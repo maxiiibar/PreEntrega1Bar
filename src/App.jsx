51a2db3f9722a2bc11bc15f1b035379685356838
Maximo Bar <maximobar2003@gmail.com>
2023-12-05 17:45:36 -0300

    Agregando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div id='app'>
       <NavBar/>
+      <ItemListContainer tituloPresentacion="PC Componentes"/>
     </div>
   )
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = ({tituloPresentacion}) => {
+  return (
+    <div>
+        <h2>Bienvenidos a {tituloPresentacion} </h2>
+    </div>
+  )
+}
+
+export default ItemListContainer
